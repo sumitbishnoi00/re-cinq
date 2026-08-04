820b6f1343bd26ccd7081f0990ac7c8d09c67204
pawandelu <pawandelu08@gmail.com>
2026-08-04 16:41:56 +0530

    hero

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Button = ({children, vari, className = "" }) => {
     const variants = {
-        pri: "font-medium w-50.25 h-12.75 border border-none bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] hover:scale-105 hover:bg-transparent flex items-center justify-center",
-        sec: "font-medium w-52.25 h-12.75 border border-border bg-transparent hover:scale-105 hover:bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] flex items-center justify-center",
+        pri: "font-medium w-50.25 h-12.75 border border-none bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] hover:scale-105  hover:bg-white/6 hover:border-border flex items-center justify-center",
+        sec: "font-medium w-52.25 h-12.75 border border-border bg-white/6 hover:scale-105 hover:bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] flex items-center justify-center",
         dan: "font-medium w-93.25 h-13 border border-border bg-transparent hover:scale-105 hover:bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] flex items-center justify-center",
         out: "font-normal py-4 px-8.5 border border-border bg-transparent hover:scale-105 hover:bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)]",
         not: "font-medium w-44.75 h-13 border border-none bg-[linear-gradient(91.05deg,#8E2EE0_7.21%,#AF7EF4_86.3%)] hover:scale-105 hover:bg-transparent flex items-center justify-center",
