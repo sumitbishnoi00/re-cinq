df23cba4ec917749a2c18702d10412fa165b9656
sumitbishnoi00 <sumitbishnoi369@gmail.com>
2026-08-04 14:25:16 +0530

    add img

---
+++ src/utils/helper.js
@@ -0,0 +1,5 @@
+export const NAV_DATA = ['Home', 'Services', 'Our Works', 'About', 'Blog',];
+
+export const INOVARTION_DROPDOWN = ['HUB', 'PODCAST', 'MONTHLY REPORT', 'PATTERNS', 'ASSESSMENT'];
+
+export const SERVICES_DROPDOWN = ['AI Readiness', 'AI Native Adoption', 'AI Native at Scale', 'AI Education'];
